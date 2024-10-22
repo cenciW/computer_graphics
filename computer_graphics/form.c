0b01c5d807c5849e895c2769d9b6d31efe028a39
Jose Augusto Cenci Castilho <ze.ccenci@gmail.com>
2024-10-22 16:51:30 +0100

    build: when click update figures and generate new figures and show the x,y from the click event.

--- computer_graphics/form.c
+++ computer_graphics/form.c
@@ -1,179 +1,166 @@
 #include "form.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <gl/glut.h>
 #include <math.h>
 
-#define PI 3.14159265
+#define PI 3.1415
 
-// this struct is encapsulated
-struct form {
-	int type;
-	float x, y;             // initial point of the form
-	float xSize, ySize;     // sides 
-	float r, g, b;
-};
 
-Form newForm(int type, float x, float y, float xSize, float ySize) {
-	Form f = (Form)malloc(sizeof(struct form));
-
+Form newForm(float x, float y, float xSize, float ySize, int type) {
+    Form f = (Form)malloc(sizeof(struct form));
 	if (f == NULL) {
-		fprintf(stderr, "Erro ao alocar memória para a nova forma.\n");
-		exit(EXIT_FAILURE);
+		printf("Error allocating memory\n");
+		exit(1);
 	}
-
 
     f->x = x;
     f->y = y;
     f->xSize = xSize;
     f->ySize = ySize;
+
+    f->r = 1.0;
+    f->g = 1.0;
+    f->b = 1.0;
+
     f->type = type;
-   
-	f->r = 1.0;
-	f->g = 1.0;
-	f->b = 1.0;
-	return f;
+
+    return f;
 }
 
-
-
-Form newFormC(int type, float x, float y, float xSize, float ySize) {
-	return newForm(type, x - (xSize / 2), y - (ySize / 2), xSize, ySize);
+Form newFormC(float x, float y, float xSize, float ySize, int type) {
+    return newForm(x - (xSize / 2), y - (ySize / 2), xSize, ySize, type);
 }
 
 Form newRectangle(float x, float y, float xSize, float ySize) {
-	
-	Form rectangle = newForm(RECTANGLE, x, y, xSize, ySize);
-	return rectangle;
+    return newForm(x, y, xSize, ySize, RECTANGLE);
 }
 
 Form newSquare(float x, float y, float side) {
-	Form square = newForm(SQUARE, x, y, side, side);
-	return square;
+    return newForm(x, y, side, side, SQUARE);
 }
 
-Form newTriangleIsos(float x, float y, float xSize, float ySize) {
-	return newForm(TRIANGLE_ISOS, x, y, xSize, ySize);
+Form newTriangleIso(float x, float y, float xSize, float ySize) {
+    return newForm(x, y, xSize, ySize, TRIANGLE_ISO);
 }
 
-Form newLine(float x, float y, float xSize, float ySize) {
-	return newForm(LINE, x, y, xSize, ySize);
+Form newHexagon(float x, float y, float xSize, float ySize) {
+    return newForm(x, y, xSize, ySize, HEXAGON);
 }
 
 Form newCircle(float x, float y, float radius) {
-	return newForm(CIRCLE,x, y, radius, radius);
-}
-
-
-void deleteForm(Form f) {
-	free(f);
-}
-
-void updateForm(Form f, float dx, float dy) {
-	f->x += dx;
-	f->y += dy;
+    return newForm(x, y, radius, radius, CIRCLE);
 }
 
 void setBackgroundColor(Form f, float r, float g, float b) {
-	f->r = r;
-	f->g = g;
-	f->b = b;
+    f->r = r;
+    f->g = g;
+    f->b = b;
+}
+
+void deleteForm(Form f) {
+    free(f);
+}
+
+void updateForm(Form f, float dx, float dy) {
+    f->x += dx;
+    f->y += dy;
 }
 
 
 void printfForm(Form f) {
-	printf("Graphical Form BBox: (%.2f,%.2f), (%.2f, %.2f), (%.2f, %.2f), (%.2f, %.2f) |\n R: %.2f, G: %.2f, B: %.2f\t\n",
-		f->x, f->y,
-		f->x, f->y + f->ySize,
-		f->x + f->xSize, f->y + f->ySize,
-		f->x + f->xSize, f->y, f->r, f->g, f->b);
+    printf("Graphical Form BBox: (%.2f,%.2f), (%.2f, %.2f), (%.2f, %.2f), (%.2f, %.2f)\t",
+        f->x, f->y,
+        f->x, f->y + f->ySize,
+        f->x + f->xSize, f->y + f->ySize,
+        f->x + f->xSize, f->y);
 
 
-	printf("Center(%.2f,%.2f) Size{%.2f,%.2f}\n",
-		f->x + (f->xSize / 2), f->y + (f->ySize / 2),
-		f->xSize, f->ySize);
-
-}
-
-void drawBox(Form f) {
-	glColor3f(f->r, f->g, f->b);
-	glBegin(GL_POLYGON);
-		glVertex2f(f->x, f->y);
-		glVertex2f(f->x, f->y + f->ySize);
-		glVertex2f(f->x + f->xSize, f->y + f->ySize);
-		glVertex2f(f->x + f->xSize, f->y);
-	glEnd();
-
-	glColor3f(rand() / (RAND_MAX * 1), rand() / (RAND_MAX * 1), rand() / (RAND_MAX * 1));
-	glBegin(GL_LINE_LOOP);
-		glVertex2f(f->x, f->y);
-		glVertex2f(f->x, f->y + f->ySize);
-		glVertex2f(f->x + f->xSize, f->y + f->ySize);
-		glVertex2f(f->x + f->xSize, f->y);
-	glEnd();
-
-	glFlush();
+    printf("Center(%.2f,%.2f) Size{%.2f,%.2f}\n",
+        f->x + (f->xSize / 2), f->y + (f->ySize / 2),
+        f->xSize, f->ySize);
 
 }
 
-void drawTriangleIso(Form f) {
-	glColor3f(f->r, f->g, f->b);
-	glBegin(GL_TRIANGLE_FAN);
-	glVertex2f(f->x, f->y);
-	glVertex2f(f->x + (f->xSize/2.0), f->y + f->ySize);
-	glVertex2f(f->x + f->xSize, f->y);
-	glEnd();
+void drawRectangle(Form f) {
+    glColor3f(f->r, f->g, f->b);
+    glBegin(GL_POLYGON);
+    glVertex2f(f->x, f->y);
+    glVertex2f(f->x, f->y + f->ySize);
+    glVertex2f(f->x + f->xSize, f->y + f->ySize);
+    glVertex2f(f->x + f->xSize, f->y);
+    glEnd();
 
-	glColor3f(rand() / (RAND_MAX * 1), rand() / (RAND_MAX * 1), rand() / (RAND_MAX * 1));
-	glBegin(GL_LINE_LOOP);
-	glVertex2f(f->x, f->y);
-	glVertex2f(f->x + (f->xSize / 2.0), f->y + f->ySize);
-	glVertex2f(f->x + f->xSize, f->y);
-	glEnd();
 
-	glFlush();
+    glColor3f((float)rand() / RAND_MAX, (float)rand() / RAND_MAX, (float)rand() / RAND_MAX);
+    glBegin(GL_LINE_LOOP);
+    glVertex2f(f->x, f->y);
+    glVertex2f(f->x, f->y + f->ySize);
+    glVertex2f(f->x + f->xSize, f->y + f->ySize);
+    glVertex2f(f->x + f->xSize, f->y);
+    glEnd();
 
+
+    glFlush();
+}
+
+void drawTriangle(Form f) {
+    glColor3f(f->r, f->g, f->b);
+    glBegin(GL_TRIANGLE_FAN);
+    glVertex2f(f->x - (f->xSize / 2), f->y - f->ySize);
+    glVertex2f(f->x, f->y);
+    glVertex2f(f->x + (f->xSize / 2), f->y - f->ySize);
+    glEnd();
+
+
+    glColor3f((float)rand() / RAND_MAX, (float)rand() / RAND_MAX, (float)rand() / RAND_MAX);
+    glBegin(GL_LINE_LOOP);
+    glVertex2f(f->x - (f->xSize / 2), f->y - f->ySize);
+    glVertex2f(f->x, f->y);
+    glVertex2f(f->x + (f->xSize / 2), f->y - f->ySize);
+    glEnd();
+
+    glFlush();
 }
 
 void drawCircle(Form f, float radius, float faces) {
 
-	float centerX = f->x + (f->xSize / 2);
-	float centerY = f->y + (f->xSize / 2);
+    float centerX = f->x + (f->xSize / 2);
+    float centerY = f->y + (f->xSize / 2);
 
-	float step = (2 * PI) / faces;
+    float step = (2 * PI) / faces;
 
-	glBegin(GL_POLYGON);
-	for (float i = 0; i < 2 * PI; i += step) {
-		float x = centerX + radius * cos(i);
-		float y = centerY + radius * sin(i);
+    glBegin(GL_POLYGON);
+    for (float i = 0; i < 2 * PI; i += step) {
+        float x = centerX + radius * cos(i);
+        float y = centerY + radius * sin(i);
 
-		glVertex2f(x, y);
-	}
-	printf("Center(%.2f,%.2f) Size{%.2f,%.2f}\n",
-		centerX, centerY,
-		radius, radius);	
+        glVertex2f(x, y);
+    }
 
-	glEnd();
-	glFlush();
+    glEnd();
+    glFlush();
 
 }
 
+
 void drawForm(Form f) {
-	switch (f->type) {
-	case RECTANGLE:
-		drawBox(f);
-		break;
-	case SQUARE:
-		drawBox(f);
-		break;
-	case TRIANGLE_ISOS:
-		drawTriangleIso(f);		
-		break;
-	case LINE:
-		//drawLine(f);
-		break;
-	case CIRCLE:
-		drawCircle(f, f->xSize / 2, 50);
-		break;
-	}
+
+    switch (f->type) {
+    case RECTANGLE:
+        drawRectangle(f);
+        break;
+    case TRIANGLE_ISO:
+        drawTriangle(f);
+        break;
+    case SQUARE:
+        drawRectangle(f);
+        break;
+    case HEXAGON:
+        drawCircle(f, f->xSize / 2, 6);
+        break;
+    case CIRCLE:
+        drawCircle(f, f->xSize / 2, 50);
+        break;
+    }
 }
--- computer_graphics/main.c
+++ computer_graphics/main.c
@@ -1,149 +1,149 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <gl/glut.h>
 #include "form.h"
-#define N 100
+#include <stdlib.h>
+#include <gl/glut.h>
+#include <math.h>
+
+#define N 10
 
 
-// Array of pointers to form objects
-Form a[N];
-Form f;
+Form figs[N];
+int cont;
+int windowX = 500;
+int windowY = 500;
 
-struct form {
-	int type;
-	float x, y;             // initial point of the form
-	float xSize, ySize;     // sides 
-	float r, g, b;          // color components
-};
+void generateRandomFigures(Form* figs, float percentual) {
 
-// Counter variable
-int counter = 0;
+    for (int i = 0; i < N; i++) {
+        if (figs[i] != NULL) {
+            deleteForm(figs[i]);
+            figs[i] = NULL;
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+
+        int figureType = (rand() % 5);
+
+        switch (figureType) {
+        case 0:
+            figs[i] = newRectangle(
+                ((float)rand() / RAND_MAX) * windowX - 1,
+                ((float)rand() / RAND_MAX) * windowY - 1,
+                ((float)rand() / RAND_MAX) * 50 - 1,
+                ((float)rand() / RAND_MAX) * 50 - 1);
+            break;
+        case 1:
+            figs[i] = newTriangleIso(
+                ((float)rand() / RAND_MAX) * windowX - 1,
+                ((float)rand() / RAND_MAX) * windowY - 1,
+                ((float)rand() / RAND_MAX) * 50 - 1,
+                ((float)rand() / RAND_MAX) * 50 - 1);
+            break;
+        case 2:
+            figs[i] = newSquare(
+                ((float)rand() / RAND_MAX) * windowX - 1,
+                ((float)rand() / RAND_MAX) * windowY - 1,
+                ((float)rand() / RAND_MAX) * 50 - 1);
+            break;
+        case 3:
+            //Hexagon
+            figs[i] = newHexagon(
+                ((float)rand() / RAND_MAX) * windowX - 1,
+                ((float)rand() / RAND_MAX) * windowY - 1,
+                ((float)rand() / RAND_MAX) * 50 - 1,
+                ((float)rand() / RAND_MAX) * 50 - 1);
+            break;
+        case 4:
+            figs[i] = newCircle(
+                ((float)rand() / RAND_MAX) * windowX - 1,
+                ((float)rand() / RAND_MAX) * windowY - 1,
+                ((float)rand() / RAND_MAX) * 50 - 1);
+            break;
+        default:
+            figs[i] = newRectangle(
+                ((float)rand() / RAND_MAX) * windowX - 1,
+                ((float)rand() / RAND_MAX) * windowY - 1,
+                ((float)rand() / RAND_MAX) * 50 - 1,
+                ((float)rand() / RAND_MAX) * 50 - 1);
+            break;
+        }
+        setBackgroundColor(figs[i], (float)rand() / RAND_MAX, (float)rand() / RAND_MAX, (float)rand() / RAND_MAX);
 
 
-int windowY = 900;
-int windowX = 900;
+    }
 
-// Function to display forms
-void display() {
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	// Draw the Y-axis and X-axis (vertical and horizontal line through the center)
-	glColor3f(1.0, 1.0, 1.0); // Set color to red for the X-axis
-	glBegin(GL_LINES);
-	glVertex2f(-1.0, 0.0); // Left side of the window
-	glVertex2f(1.0, 0.0);  // Right side of the window
-	glVertex2f(0.0, -1.0); // Bottom of the window
-	glVertex2f(0.0, 1.0);  // Top of the window
-	glEnd();
-
-	// Draw the forms
-	for (int i = 0; i < N; i++) {
-		drawForm(a[i]);
-	}
-
-	glFlush();
-
-	// Increment and display the counter value
-	counter++;
-	printf("Counter: %d\n", counter);
 }
 
-Form createRandomForm() {
-	Form f;
-	switch (rand() % 5)
-	{
-	case 0:
-		f = newRectangle(((float)rand() / RAND_MAX * windowX),
-				((float)rand() / RAND_MAX * windowY),
-			((float)rand() / RAND_MAX) * 50 - 1,
-			((float)rand() / RAND_MAX) * 50 - 1);
-		break;
-	case 1:
-		f = newSquare(((float)rand() / RAND_MAX * windowX),
-					((float)rand() / RAND_MAX * windowY),
-			((float)rand() / RAND_MAX)*50 - 1);
-			
-		break;
-	case 2:
-		f = newTriangleIsos(((float)rand() / RAND_MAX * windowX),
-				((float)rand() / RAND_MAX * windowY),
-			((float)rand() / RAND_MAX) * 50 - 1,
-			((float)rand() / RAND_MAX) * 50 - 1);
-		break;
-	case 3:
-		f = newLine(((float)rand() / RAND_MAX * windowX),
-				((float)rand() / RAND_MAX * windowY),
-				rand() % windowX,
-				rand() % windowY);
-		break;
-	case 4:
-		f = newCircle(((float)rand() / RAND_MAX * windowX),
-				((float)rand() / RAND_MAX * windowY),
-			((float)rand() / RAND_MAX) * 50 - 1);
-		break;
-	default:
-		f = newRectangle(((float)rand() / RAND_MAX * windowX),
-				((float)rand() / RAND_MAX * windowY),
-			((float)rand() / RAND_MAX) * 50 - 1,
-			((float)rand() / RAND_MAX) * 50 - 1);
-		break;
-	}
-	setBackgroundColor(f, (float)rand() / RAND_MAX, (float)rand() / RAND_MAX, (float)rand() / RAND_MAX);
+void mouseClick(int button, int state, int x, int y) {
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		printf("Clicou na posicao (%d, %d)\n", x, y);
+        generateRandomFigures(figs, 100);
+		glutPostRedisplay();
+    }
+}
 
-	return f;
 
+void displayForms() {
+    srand(time(NULL));
+    glClear(GL_COLOR_BUFFER_BIT);
+
+
+    generateRandomFigures(figs, 90);
+
+    cont++;
+
+    printf("Contador: %d\n", cont);
+
+    for (int i = 0; i < N; i++) {
+        if (figs[i] != NULL) {
+            drawForm(figs[i]);
+        }
+    }
 }
 
 void init(int width, int height) {
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0.0, width, height, 0.0, -1.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0, width, height, 0.0, -1.0, 1.0);
 }
 
-void reshape(int w, int h) {
-	glViewport(0, 0, w, h);
-	init(w, h);
-	//glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(0.0, (GLdouble)w, 0.0, (GLdouble)h);
-	windowX = w;
-	windowY = h;
+void reShape(int width, int height) {
+    glViewport(0, 0, width, height);
+    init(width, height);
 }
 
-void configOpenGL() {
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(windowX, windowY);
-	glutInitWindowPosition(0, 0);
-	glutCreateWindow("Tela de figuras malucas");
-	init(windowX, windowY);
-	glutDisplayFunc(display);
-	glutReshapeFunc(reshape);
-	glutMainLoop();
+void setupOpenGL() {
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitWindowSize(windowX, windowY);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("Janela para figs");
+    init(windowX, windowY);
+    glutDisplayFunc(displayForms);
+    glutReshapeFunc(reShape);
+    glutMouseFunc(mouseClick);
+    glutMainLoop();
 }
-
-
-
 
 int main(int argc, char** argv) {
-	for (int i = 0; i < N; i++) {
-		a[i] = NULL;
-	}
+    for (int i = 0; i < N; i++) {
+        figs[i] = NULL;
+    }
+
+    srand(time(NULL));
+    generateRandomFigures(figs, 70);
+    cont = 0;
 
 
-	srand(time(NULL));
-	//generateRandomForms(a, 70);
+    for (int i = 0; i < N; i++) {
+        if (figs[i] != NULL) {
+            printf("Figura %d: ", i);
+            printfForm(figs[i]);
+            printf("\n");
+        }
+    }
 
-	for (int i = 0; i < N; i++) {
-		a[i] = createRandomForm();
-		if (a[i] != NULL) {
-			printf("Figura %d: ", i);
-			printfForm(a[i]);
-			printf("\n");
-		}
-	}
+    glutInit(&argc, argv);
+    setupOpenGL();
 
-	glutInit(&argc, argv);
-	configOpenGL();
-
-	return 0;
+    return 0;
 }
